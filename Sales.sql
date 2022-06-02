a5e5368854bf4d99ce088bcb02037d23b5facf18
Mansour Achaour <achaourmansour@gmail.com>
2022-06-02 16:55:20 +0200

    2. Insertions

--- Sales.sql
+++ Sales.sql
@@ -3,7 +3,7 @@ USE Sales;
 SHOW TABLES;
 
 
--- ## Créer une table appeléetelephonesavec les colonnes
+-- ## Créer une table appelée telephone savec les colonnes
 suivantes :
 -- *=>name:chaîne de caractèresqui représentele modèle du téléphone
 -- *=>manufacturer:chaîne de caractèresquireprésente le fabricant
@@ -22,12 +22,12 @@ CREATE TABLE telephones (
 -- # LMD 1 : Insertions
 -- *=>Insérer les données suivantes :
 
-INSERT INTO clients (name, manufacturer, price, units_sold)
-values("N1280","Nokia",199,1925)
+INSERT INTO telephones (name, manufacturer, price, units_sold)
+values("N1280","Nokia",199,1925),
 ("Iphone 4","Apple",399,9436),
-("Galaxy S 5","Samsung",299,2359)
-("S5620 Monte","Samsung",250,2385)
-("N8","Nokia",150,7543)
-("Droid","Motorola",150,8395)
-("Iphone 13 ProMax","Apple",1300,12849)
-("Galaxy Note 20","Samsung",1100,10353)
+("Galaxy S 5","Samsung",299,2359),
+("S5620 Monte","Samsung",250,2385),
+("N8","Nokia",150,7543),
+("Droid","Motorola",150,8395),
+("Iphone 13 ProMax","Apple",1300,12849),
+("Galaxy Note 20","Samsung",1100,10353);
