9848d39e86fa1eb9d8c77b01ea13ba047e64bb87
Mansour Achaour <achaourmansour@gmail.com>
2022-06-02 10:56:40 +0200

    Creation de la base de données

---
+++ clients.sql
@@ -0,0 +1,14 @@
+-- Authentification/Connexion
+>mysql -u root    -- si l'utilisateur n'a pas de mot de passe
+>mysql -u root -p -- Si l'utilisateur 
+
+-- 1. lister toutes les bases de données
+SHOW DATABASES;
+
+
+-- 2. Creation de la base de données.
+
+CREATE DATABASE Ventes;
+
+
+-- 3. Creation de la table de données 
