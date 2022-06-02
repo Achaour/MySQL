56104dee13639d0403fcb902931366eb14b5fe68
Mansour Achaour <achaourmansour@gmail.com>
2022-06-02 12:01:54 +0200

    4. Creation d'une table client

--- clients.sql
+++ clients.sql
@@ -10,8 +10,29 @@ SHOW DATABASES;
 
 CREATE DATABASE Ventes;
 
--- 3. Utiliser/Selctionner une base de données.
-
+-- 3. Utiliser/sélectionner une base de données.
 USE ventes;
 
--- 4. Creation de la table de données 
+-- 4. lister tous les tables 
+
+SHOW TABLES;
+
+-- 5. Creation de la table de données 
+
+-- INTEGER: LE TYPE ENTIER
+-- NOT NULL: Cette colonne doit toujours contenir une information
+-- AUTO_INCREMENT: Dire à MYSQL de fournir de valeurs en incrementant automatiquement des cles
+-- PRIMARY KEY: marquer la colonne comme étant la clé primaire
+-- UNIQUE: Applique une contrainte de d'unicité
+
+CREATE TABLE clients (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR (50) NOT NULL,
+    email VARCHAR (70) UNIQUE ,
+    telephone VARCHAR (20) NOT NULL,  
+);
+
+
+
+
